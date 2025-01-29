3c902be3d309652f0668987a220a0482bc4a1c8c
oliver <o.kohl@sap.com>
2025-01-29 22:51:41 +0100

    fonts and forms
    - Lato font added
    - contact form added (draft)
    - small layout changes

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy({ "src/public": "." });
 	eleventyConfig.addWatchTarget("**/*.(png|jpeg|webp|js)");
 
