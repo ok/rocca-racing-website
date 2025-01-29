931e6cc87e5079f2a073603137ec1973b983cf03
oliver <o.kohl@sap.com>
2025-01-29 12:58:45 +0100

    img and sitemap
    - sitemap generator added
    - favicon moved to public (static)
    - images moved to img

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 export default function(eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
   eleventyConfig.setOutputDirectory("dist");
+
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/public");
-  eleventyConfig.addPassthroughCopy({ "src/img/favicon.ico": "/" });
-	eleventyConfig.addWatchTarget("**/*.(png|jpeg|webp|ico)");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy({ "src/public": "." });
+	eleventyConfig.addWatchTarget("**/*.(png|jpeg|webp)");
 };
